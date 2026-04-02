e61e7207cd25b6441d00a9c92da74f81ed915cbe
Developer <developer@bank-it-arch.local>
2026-04-02 14:50:36 +0800

    feat: add mappers for oss use standing book

---
+++ bank-it-arch-backend/src/main/java/com/bank/itarch/mapper/OssUseStandingBookMainMapper.java
@@ -0,0 +1,9 @@
+package com.bank.itarch.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.bank.itarch.model.entity.OssUseStandingBookMain;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OssUseStandingBookMainMapper extends BaseMapper<OssUseStandingBookMain> {
+}
